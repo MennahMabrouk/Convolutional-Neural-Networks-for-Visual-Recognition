f4f9cea8b5a48815fae2122a43fa09ea0c61ba8c
MennahTullah Sameh <101124995+MennahMabrouk@users.noreply.github.com>
2023-03-21 06:12:13 +0200

    Update super_resolution_sub_pixel.py

--- super_resolution_sub_pixel/super_resolution_sub_pixel.py
+++ super_resolution_sub_pixel/super_resolution_sub_pixel.py
@@ -21,26 +21,31 @@ for image super-resolution. The model takes an input image and applies a series 
 by a final depth-to-space layer that upscales the image by the specified factor. 
 The model architecture is based on the "ESPCN" model for super-resolution.
 '''
-import tensorflow as tf
-import os
-import numpy as np
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.preprocessing.image import load_img
-from tensorflow.keras.preprocessing.image import array_to_img
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.preprocessing import image_dataset_from_directory
-from IPython.display import display
+import tensorflow as tf             # Importing TensorFlow library
+import os                            # Importing Operating System module
+import numpy as np                   # Importing NumPy library
+from tensorflow import keras        # Importing Keras from TensorFlow
+from tensorflow.keras import layers # Importing Layers module from Keras
+from tensorflow.keras.preprocessing.image import load_img, array_to_img, img_to_array   # Importing image pre-processing modules from Keras
+from tensorflow.keras.preprocessing import image_dataset_from_directory   # Importing image dataset creation module from Keras
+from IPython.display import display  # Importing display module from IPython
 
+# Download dataset from url and extract data directory path
 dataset_url = "http://www.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/BSR/BSR_bsds500.tgz"
 data_dir = keras.utils.get_file(origin=dataset_url, fname="BSR", untar=True)
 root_dir = os.path.join(data_dir, "BSDS500/data")
 
-crop_size = 300
-upscale_factor = 3
-input_size = crop_size // upscale_factor
-batch_size = 8
+# Set variables for image cropping, upscaling, and batch size
+crop_size = 300         # Setting the crop size of images
+upscale_factor = 3      # Setting the scale factor for image upscaling
+input_size = crop_size // upscale_factor  # Calculating input size of images after downscaling
+batch_size = 8          # Setting the batch size for training dataset
 
+'''
+This code creates training and validation datasets using the image_dataset_from_directory function and performs pre-processing on them
+'''
+
+# Load training dataset from directory
 train_ds = image_dataset_from_directory(
     root_dir,
     batch_size=batch_size,
@@ -51,6 +56,7 @@ train_ds = image_dataset_from_directory(
     label_mode=None,
 )
 
+# Load validation dataset from directory
 valid_ds = image_dataset_from_directory(
     root_dir,
     batch_size=batch_size,
@@ -61,12 +67,13 @@ valid_ds = image_dataset_from_directory(
     label_mode=None,
 )
 
+# Function for scaling input image pixels to between 0 and 1
 def scaling(input_image):
-    input_image = input_image / 255.0
+    input_image = input_image / 255.0  # Scaling the input image to a range of [0,1]
     return input_image
 
-train_ds = train_ds.map(scaling)
-valid_ds = valid_ds.map(scaling)
+train_ds = train_ds.map(scaling)  # Applying scaling to training dataset
+valid_ds = valid_ds.map(scaling)  # Applying scaling to validation dataset
 
 dataset = os.path.join(root_dir, "images")
 test_path = os.path.join(dataset, "test")
@@ -78,6 +85,10 @@ test_img_paths = sorted(
         if fname.endswith(".jpg")
     ]
 )
+
+'''
+This code defines functions for processing the input and target images, maps those functions to the training and validation datasets
+'''
 
 def process_input(input, input_size, upscale_factor):
     input = tf.image.rgb_to_yuv(input)
